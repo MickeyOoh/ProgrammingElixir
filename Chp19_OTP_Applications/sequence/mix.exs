eb3f16acf828a6c6d27c9e3db18a5a93572455dc
MickeyOoh <mikio.westvil@gmail.com>
2018-03-16 11:31:15 -0400

    Chp20_end

--- Chp19_OTP_Applications/sequence/mix.exs
+++ Chp19_OTP_Applications/sequence/mix.exs
@@ -29,7 +29,7 @@ defmodule Sequence.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:exrm, "~> 1.0.6"}
+      {:exrm, "~> 1.0.6"}
     ]
   end
 end
--- Chp20_TaskAgent/task1.exs
+++ Chp20_TaskAgent/task1.exs
@@ -2,6 +2,17 @@ defmodule Fib do
   def of(0), do: 0
   def of(1), do: 1
   def of(n), do: Fib.of(n-1) + Fib.of(n-2)
+  def start(n) do 
+    IO.puts "2:start the Task.async"
+    worker = Task.async(fn -> of(n) end)
+    IO.puts "2:Do something else"
+    IO.puts "2:Wait for the task"
+    result = Task.await(worker)
+    IO.puts "2:The result is #{result}"
+  end
+  def wait(pid) do 
+    result = Task.await(pid)
+  end
 end
 
 IO.puts "Start the task"
@@ -13,3 +24,7 @@ result = Task.await(worker)
 
 IO.puts "The result is #{result}"
 
+IO.puts "**** "
+n = 20
+{time, result} = :timer.tc(Fib, :start, [n])
+:io.format "Fib.of(~2B)     ~.3f(ms)~n", [n, time/1000.0]
